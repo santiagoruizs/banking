9c7ad7c3928a3782391ea627f0cbcb956598121e
Santiago Ruiz <sruizs@Santiagos-Laptop.local>
2024-07-17 20:45:44 +0200

    adds basic main layout information and display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Santiago'}
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className='home-content'>
+        <header className='home-header'>
+
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+        <TotalBalanceBox 
+        accounts = {[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.35}
+
+/>
+      </div>
+    </div>
   )
 }
 
---
+++ components/ui/AnimatedCounter.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from 'react'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}:{amount: number}) => {
+  return (
+    <div className='w-full'>
+        <CountUp 
+            end={amount} 
+            decimal='.' 
+            decimals={2} 
+            prefix="$"
+            duration={1}
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
